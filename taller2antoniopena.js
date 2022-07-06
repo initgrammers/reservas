c9c583ba3e2e8879a8b0c394e735cd4d2231f1fd
Antonio Peña <sirantoniog@gmail.com>
2022-07-06 04:14:56 -0500

    feat: mostrar en tabla reservas por habitacion con marcas de entrada y salida

--- taller2antoniopena.js
+++ taller2antoniopena.js
@@ -1,6 +1,6 @@
 // Como: usuario administrador
 // Quiero: poder visualizar las reservas
-//      agrupdas por habitación marcadas la entrada y salida
+//      agrupadas por habitación marcadas la entrada y salida
 //      en un calendario
 // Para: poder visualizar la ocupación del edificio de 
 // manera más amigable
@@ -37,12 +37,47 @@ const bookings = [
     },
 ];
 
-  // OUTPUT:
+// OUTPUT:
+let habitacion1 = bookings.filter(item => item.room === "Habitación 1");
+let habitacion2 = bookings.filter(item => item.room === "Habitación 2");
+let habitacion3 = bookings.filter(item => item.room === "Habitación 3");
 
-  // console.table(resp);
+let maxDays = bookings.map(item => item.to);
+maxDays = Math.max(...maxDays);
 
-console.table({
-    "Habitación 1": ["", "⬇️Aang", "Aang", "⬆️Aang ⬇️Iroh", "Iroh", "⬆️Iroh"],
-    "Habitación 2": ["", "⬇️Toph", "Toph", "Toph", "Toph", "⬆️Toph"],
-    "Habitación 3": ["", "", "⬇️Sokka", "Sokka", "⬆️Sokka", ""],
-});
+let arrDays = [];
+for(let i = 0; i <= maxDays; i++){
+    arrDays[i] = "";
+}
+
+let huespedesxHabitacion = (habitacion, arrDays) => {
+    let huespedes = habitacion.reduce((arr,item) => {
+        for (let i = 0; i <= item.to; i++) {
+            i >= item.from && i <= item.to ?
+            i === item.from ? arr[i] = arr[i] + ' '+ '⬇️' +item.owner :
+            i === item.to ? arr[i] = arr[i] + ' '+ '⬆️' +item.owner :
+            arr[i] = arr[i] + ' ' +item.owner : arr[i] = arr[i];
+        }
+        return arr;
+    },[...arrDays]);
+    huespedes = huespedes.map(element => element.trim());
+    return huespedes;
+}
+
+let huespedesHabitacion1 = huespedesxHabitacion(habitacion1, arrDays);
+let huespedesHabitacion2 = huespedesxHabitacion(habitacion2, arrDays);
+let huespedesHabitacion3 = huespedesxHabitacion(habitacion3, arrDays);
+
+let resp = {
+    'Habitacion 1': huespedesHabitacion1,
+    'Habitacion 2': huespedesHabitacion2,
+    'Habitacion 3': huespedesHabitacion3,
+}
+
+console.table(resp);
+
+// console.table({
+//     "Habitación 1": ["", "⬇️Aang", "Aang", "⬆️Aang ⬇️Iroh", "Iroh", "⬆️Iroh"],
+//     "Habitación 2": ["", "⬇️Toph", "Toph", "Toph", "Toph", "⬆️Toph"],
+//     "Habitación 3": ["", "", "⬇️Sokka", "Sokka", "⬆️Sokka", ""],
+// });
