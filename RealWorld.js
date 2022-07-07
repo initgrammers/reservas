052d2fd763480ee459868142695160825663312f
Andrés Morales <moralesandres99@gmail.com>
2022-07-07 17:59:30 -0500

    Taller 2

--- RealWorld.js
+++ RealWorld.js
@@ -39,7 +39,41 @@ const bookings = [
 
 // OUTPUT:
 
-// console.table(resp);
+function Habitacion(dias) {
+  this.dia1 = dias.dia1;
+  this.dia2 = dias.dia2;
+  this.dia3 = dias.dia3;
+  this.dia4 = dias.dia4;
+  this.dia5 = dias.dia5;
+  this.dia6 = dias.dia6;
+  this.dia7 = dias.dia7;
+}
+
+var reservas = {};
+
+const boocking = bookings.filter(({room}) => room === "Habitación 1");
+
+
+reservas.habitacion1 = new Habitacion(boocking.map(({ owner }) => owner).fill(boocking.owner, boocking.from+1, boocking.to-1).fill("", 0,boocking.from-1).fill("", boocking.to+1, 5).fill("⬇️"+boocking.owner, boocking.from).fill("⬆️"+ boocking.owner, boocking.to));
+
+
+const boocking1 = bookings.filter(({ room}) => room ==="Habitación 2");
+
+
+reservas.habitacion2 = new Habitacion(boocking1.map(({ owner }) => owner).fill(boocking.owner, boocking.from+1, boocking.to-1).fill("", 0,boocking.from-1).fill("", boocking.to+1, 5).fill("⬇️"+boocking.owner, boocking.from).fill("⬆️"+ boocking.owner, boocking.to));
+
+
+const boocking2 = bookings.filter(({ room}) => room ==="Habitación 3");
+
+
+reservas.habitacion3 = new Habitacion(boocking2.map(({ owner }) => owner).fill(boocking.owner, boocking.from+1, boocking.to-1).fill("", 0,boocking.from-1).fill("", boocking.to+1, 5).fill("⬇️"+boocking.owner, boocking.from).fill("⬆️"+ boocking.owner, boocking.to));
+
+
+console.table(reservas);
+
+
+
+//console.table(resp);
 
 console.table({
   "Habitación 1": ["", "⬇️Aang", "Aang", "⬆️Aang ⬇️Iroh", "Iroh", "⬆️Iroh"],
